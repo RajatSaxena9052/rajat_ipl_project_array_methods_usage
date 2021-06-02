332382e47bd4edaaf2e829427319aa23b41b9008
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-06-03 00:44:02 +0530

    Added spaces minor change

--- src/server/outPutFileGenerator.js
+++ src/server/outPutFileGenerator.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+
 function outPutFileGenerator(filePath, data, message) {
+
     fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', (error) => {
         if (error) {
             return error;
@@ -7,5 +9,6 @@ function outPutFileGenerator(filePath, data, message) {
             console.log(message);
         }
     });
+
 }
 module.exports = outPutFileGenerator;
