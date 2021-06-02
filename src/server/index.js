d7e15650b6797e21ba1e6477e9adb503a61a7b39
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-06-03 00:38:30 +0530

    Added outputFileGenerator module
    -removed all the fs module from index.js
    -made different function calls to one fs module

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const csvToJson = require("csvtojson");
-const fs = require('fs');
+const outputFileGenerator = require("./outPutFileGenerator");
+
 
 const matches = "src/data/matches.csv";
 const deliveries = "src/data/deliveries.csv";
@@ -41,78 +42,23 @@ function main() {
 
 function convertAndSave(calculatedValues) {
 
-    fs.writeFile("src/public/output/matchesPerYear.json", JSON.stringify(calculatedValues.matchPlayedPerYear, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile matchesPerYear.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/matchesPerYear.json", calculatedValues.matchPlayedPerYear, "outputfile matchesPerYear.json is created");
 
-    fs.writeFile("src/public/output/matchesWonPerTeam.json", JSON.stringify(calculatedValues.matchWonPerTeam, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile matchesWonPerTeam.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/matchesWonPerTeam.json", calculatedValues.matchWonPerTeam, "outputfile matchesWonPerTeam.json is created");
 
-    fs.writeFile("src/public/output/extraRuns2016.json", JSON.stringify(calculatedValues.extraRuns, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile extraRuns2016.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/extraRuns2016.json", calculatedValues.extraRuns, "outputfile extraRuns2016.json is created");
 
-    fs.writeFile("src/public/output/economicalBowlers2015.json", JSON.stringify(calculatedValues.tenEconomicPlayer, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile economicalBowlers2015.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/economicalBowlers2015.json", calculatedValues.tenEconomicPlayer, "outputfile economicalBowlers2015.json is created");
 
-    fs.writeFile("src/public/output/teamsWonTossMatch.json", JSON.stringify(calculatedValues.wonTossAndMatch, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile teamsWonTossMatch.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/teamsWonTossMatch.json", calculatedValues.wonTossAndMatch, "outputfile teamsWonTossMatch.json is created");
 
-    fs.writeFile("src/public/output/playerOfMatch.json", JSON.stringify(calculatedValues.winnerOfPlayerOfMatch, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile playerOfMatch.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/playerOfMatch.json", calculatedValues.winnerOfPlayerOfMatch, "outputfile playerOfMatch.json is created");
 
-    fs.writeFile("src/public/output/strikeRate.json", JSON.stringify(calculatedValues.strikeRate, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile strikeRate.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/strikeRate.json", calculatedValues.strikeRate, "outputfile strikeRate.json is created");
 
-    fs.writeFile("src/public/output/highestDismissedPlayer.json", JSON.stringify(calculatedValues.playerDismissed, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile highestDismissedPlayer.json is created");
-        }
-    });
+    outputFileGenerator("src/public/output/highestDismissedPlayer.json", calculatedValues.playerDismissed, "outputfile highestDismissedPlayer.json is created");
 
-    fs.writeFile("src/public/output/bestSuperOverBowler.json", JSON.stringify(calculatedValues.superOverEconomy, null, 2), 'utf8', (error) => {
-        if (error) {
-            return error;
-        } else {
-            console.log("outputfile bestSuperOverBowler.json is created");
-        }
-    });
-
+    outputFileGenerator("src/public/output/bestSuperOverBowler.json", calculatedValues.superOverEconomy, "outputfile bestSuperOverBowler.json is created");
 }
 
 main();
