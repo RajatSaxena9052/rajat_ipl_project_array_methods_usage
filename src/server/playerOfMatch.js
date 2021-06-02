47904e6895dbd27d17206f0fb8505691d61a3a3b
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-06-02 18:14:22 +0530

    Added highest player of match award winner

---
+++ src/server/playerOfMatch.js
@@ -0,0 +1,47 @@
+function PlayerOfMatch(matchesArray) {
+    if (matchesArray !== undefined) {
+
+        let allMatchYear = matchesArray.reduce((accumulator, matches) => {
+            let year = matches.season;
+
+            accumulator.add(year);
+
+            return accumulator;
+
+        }, new Set());
+
+        let playerOfMatch = [...allMatchYear].reduce((accumulator, season) => {
+
+            let playerPerSeason = matchesArray.reduce((accumulator, matches) => {
+                let playerName = matches.player_of_match;
+                let year = matches.season;
+
+                if (season == year) {
+                    if (accumulator[playerName] === undefined) {
+                        accumulator[playerName] = 1;
+                    } else {
+                        accumulator[playerName] += 1;
+                    }
+                }
+
+                return accumulator;
+
+            }, {});
+
+            let maxPlayerOfMatch = Object.entries(playerPerSeason)
+                .sort((player1, player2) => {
+                    return player2[1] - player1[1];
+                })
+
+            accumulator[season] = maxPlayerOfMatch[0][0];
+
+            return accumulator;
+
+        }, {});
+
+        return playerOfMatch;
+    }
+
+    return {};
+}
+module.exports = PlayerOfMatch
